fa67a85bd779667e549db98ffec6dcee9ad0ce3d
VephaR <vephar@163.com>
2025-05-08 18:54:40 +0800

    Update chat.go

--- plugin/chat/chat.go
+++ plugin/chat/chat.go
@@ -9,18 +9,22 @@ import (
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
 	zero "github.com/wdvxdr1123/ZeroBot"
-	"github.com/wdvxdr1123/ZeroBot/extension/rate"
+	// "github.com/wdvxdr1123/ZeroBot/extension/rate"
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
 var (
-	poke   = rate.NewManager[int64](time.Minute*5, 8) // 戳一戳
+	// poke   = rate.NewManager[int64](time.Minute*5, 8) // 戳一戳
 	engine = control.AutoRegister(&ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
 		Brief:            "基础反应, 群空调",
 		Help:             "chat\n- [BOT名字]\n- [戳一戳BOT]\n- 空调开\n- 空调关\n- 群温度\n- 设置温度[正整数]",
 	})
 )
+
+func randText(text ...string) message.Segment {
+	return message.Text(text[rand.Intn(len(text))])
+}
 
 func init() { // 插件主体
 	// 被喊名字
@@ -41,87 +45,34 @@ func init() { // 插件主体
 	engine.On("notice/notify/poke", zero.OnlyToMe).SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
 			var nickname = zero.BotConfig.NickName[0]
-			rand.Seed(time.Now().UnixNano())
-			r := rand.Intn(26)
-			if r == 0 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("请不要戳", nickname, " >_<"))
-			} else if r == 1 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("喂(#`O′) 戳", nickname, "干嘛！"))
-			} else if r == 2 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("戳坏了", nickname, "，你赔得起吗？"))
-			} else if r == 3 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("awa，好舒服呀(bushi)"))
-			} else if r == 4 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("嗯……不可以……啦……不要乱戳"))
-			} else if r == 5 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("喂，110吗，有人老戳我！"))
-			} else if r == 6 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("再戳我让你变成女孩子喵！"))
-			} else if r == 7 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("不要再戳了呜呜……(害怕ing)"))
-			} else if r == 8 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("还戳，哼(つд⊂)(生气)"))
-			} else if r == 9 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("我要生气惹！o(>﹏<)o"))
-			} else if r == 10 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("呃啊啊啊~戳坏了……"))
-			} else if r == 11 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("再戳我我就把你吃掉喵！"))
-			} else if r == 12 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("正在定位您的真实地址……定位成功。轰炸机已经起飞喵！炸似你喵！"))
-			} else if r == 13 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("我给你超超，球球别再戳我了qwq"))
-			} else if r == 14 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("别再戳我了喵……"))
-			} else if r == 15 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("放手啦，不给戳QAQ"))
-			} else if r == 16 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("涩批，你再戳咬你喵！"))
-			} else if r == 17 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("再戳", nickname, "，我要叫我主人了！"))
-			} else if r == 18 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("欸很烦欸！你戳🔨呢你"))
-			} else if r == 19 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("啊呜，你有什么心事吗？"))
-			} else if r == 20 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("啊呜，太舒服刚刚竟然睡着了w有什么事喵？"))
-			} else if r == 21 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("检测到您的手速超标喵！正在启动反向拆家程序——你的小鱼干即将被没收喵qwq"))
-			} else if r == 22 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("侦测到持续骚扰行为喵！再伸手让你变成猫娘！"))
-			} else if r == 23 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("脸颊温度过热警告！继续戳真的要哭给你看喵！o(>﹏<)o"))
-			} else if r == 24 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("您的戳戳行为导致表情管理系统崩坏！都是你的错喵！"))
-			} else if r == 25 {
-				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("检测到指纹残留痕迹！跑不掉的喵！"))
-			}
+			ctx.SendChain(randText(
+				"请不要戳", nickname, " >_<",
+				"喂(#`O′) 戳", nickname, "干嘛！",
+				"戳坏了", nickname, "，你赔得起吗？",
+				"awa，好舒服呀(bushi)",
+				"嗯...不可以...啦...不要乱戳",
+				"喂，110吗，有人老戳我！",
+				"再戳我让你变成女孩子喵！",
+				"不要再戳了呜呜...(害怕ing)",
+				"还戳，哼(つд⊂)(生气)",
+				"我要生气惹！o(>﹏<)o",
+				"呃啊啊啊~戳坏了...",
+				"再戳我我就把你吃掉喵！",
+				"正在定位您的真实地址...定位成功。轰炸机已经起飞喵！炸似你喵！",
+				"我给你超超，球球别再戳我了qwq",
+				"别再戳我了喵...",
+				"放手啦，不给戳QAQ",
+				"涩批，你再戳咬你喵！",
+				"再戳", nickname, "，我要叫我主人了！",
+				"欸很烦欸！你戳🔨呢你",
+				"啊呜，你有什么心事吗？",
+				"啊呜，太舒服刚刚竟然睡着了w有什么事喵？",
+				"检测到持续骚扰行为喵！你的小鱼干即将被没收喵qwq",
+				"气似我了喵！再伸手就离家出走喵！",
+				"脸颊温度过热警告！继续戳真的要哭给你看喵！o(>﹏<)o",
+				"您的戳戳行为导致表情管理系统崩坏！都是你的错喵！",
+				"检测到指纹残留痕迹！跑不掉的喵！",
+			))
 		})
 	// 群空调
 	var AirConditTemp = map[int64]int{}
